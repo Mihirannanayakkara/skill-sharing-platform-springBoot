7f92f78769c07a98bd54c5c09629e2147db6bb9b
Rahul Nanayakkara <tharindatheekshana1@gmail.com>
2025-05-14 15:48:38 +0530

    Google-Auth-Fixed

--- backend/src/main/java/com/myproject/backend/repository/UserRepository.java
+++ backend/src/main/java/com/myproject/backend/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.myproject.backend.repository;
 import com.myproject.backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username); // For checking existing username during login
+    Optional<User> findByEmail(String email);
 }
